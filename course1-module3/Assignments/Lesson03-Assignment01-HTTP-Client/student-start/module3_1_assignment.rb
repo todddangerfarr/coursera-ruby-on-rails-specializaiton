5d509ff5dfd68a28f3ddaadfb8444d40a1b4d3fa
Todd Farr <todd.farr@gmail.com>
2016-09-24 13:30:54 +0900

    Completed Course 1 Module 3 practice assignment.

--- course1-module3/Assignments/Lesson03-Assignment01-HTTP-Client/student-start/module3_1_assignment.rb
+++ course1-module3/Assignments/Lesson03-Assignment01-HTTP-Client/student-start/module3_1_assignment.rb
@@ -1,5 +1,14 @@
 require 'httparty'
 
 class Recipe
+  include HTTParty
+  ENV["FOOD2FORK_KEY"] = 'key-goes-here' # changed for commit 
 
+  base_uri "http://food2fork.com/api"
+  default_params key: ENV["FOOD2FORK_KEY"]
+  format :json
+
+  def self.for (keyword)
+    get("/search", query: {q: keyword})["recipes"]
+  end
 end
--- course1-module3/my_first_app/app/controllers/courses_controller.rb
+++ course1-module3/my_first_app/app/controllers/courses_controller.rb
@@ -1,6 +1,6 @@
 class CoursesController < ApplicationController
   def index
-    @search_term = 'jhu'
+    @search_term = params[:looking_for] || 'jhu'
     @courses = Coursera.for(@search_term)
   end
 end
